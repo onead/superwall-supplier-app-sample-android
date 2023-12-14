4882c592271753e5ced0f2a6048a92a4793d4a45
Brady Chen <bradychen@onead.com.tw>
2023-12-14 11:57:27 +0800

    fix webview location.href 會另開視窗

--- app/src/main/java/tw/com/one2direct/superwall_sample/WebViewActivity.kt
+++ app/src/main/java/tw/com/one2direct/superwall_sample/WebViewActivity.kt
@@ -2,7 +2,10 @@ package tw.com.one2direct.superwall_sample
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebChromeClient
 import android.webkit.WebView
+import android.webkit.WebViewClient
+
 @SuppressLint("SetJavaScriptEnabled")
 class WebViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,12 +13,20 @@ class WebViewActivity : AppCompatActivity() {
         setContentView(R.layout.activity_web_view)
         intent.getStringExtra("url")?.let { webViewInit(it) }
         findViewById<android.widget.Button>(R.id.buttonClose).setOnClickListener { finish() }//請實作關閉任務牆
+
     }
 
     private fun webViewInit(url: String) {
         val webView= findViewById<WebView>(R.id.webView)
         webView.settings.javaScriptEnabled = true// 必須啟用JavaScript
         webView.settings.domStorageEnabled = true// 必須啟用domStorage
+        webView.webChromeClient = object: WebChromeClient() {};
+        webView.webViewClient = object: WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url!!)
+                return true
+            }
+        }
         webView.loadUrl(url)
     }
 }
